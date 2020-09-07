8183ef3296c3db6279ab44d72c1f25089851bb6f
Radu Suciu <radusuciu@gmail.com>
2020-09-07 16:25:22 -0700

    Refactor output to file/stdout into separate class

--- traktor_nowplaying/core.py
+++ traktor_nowplaying/core.py
@@ -45,33 +45,64 @@ def create_request_handler(callbacks):
 
     return TraktorHandler
 
-def _get_track_string(data):
-    info = dict(data)
-    track_string = f'{info.get("artist", "")} - {info.get("title", "")}'
-    return track_string if len(track_string) > 3 else ''
 
-def _output_to_console(data):
-    track_string = _get_track_string(data)
-    if track_string:
-        print(track_string)
+class TrackWriter:
+    """Writes tracks to standard output and/or file."""
+    def __init__(self, quiet=QUIET, outfile=None, append=APPEND, max_tracks=MAX_TRACKS):
+        self.quiet = quiet
+        self.outfile = outfile
+        self.append = append
+        self.max_tracks = max_tracks
 
-def _output_to_file(data, outfile, append=False, max_tracks=-1):
-    mode = 'a' if (append and max_tracks < 0)  else 'w'
+        if self.outfile:
+            try:
+                self._create_outfile()
+            except:
+                print(f'Error encountered while trying to write to {self.outfile}.')
 
-    current_track_string = _get_track_string(data)
-    tracks_to_output = [current_track_string]
+    def update(self, data):
+        if not self.quiet:
+            self._to_stdout(data)
+        if self.outfile:
+            self._to_file(data)
 
-    if max_tracks > 1:
-        with open(outfile, 'r') as f:
-            existing_tracks = f.read().splitlines()
+    def _get_track_string(self, data):
+        info = dict(data)
+        track_string = f'{info.get("artist", "")} - {info.get("title", "")}'
+        return track_string if len(track_string) > 3 else ''
 
-        tracks_to_output = [
-            *existing_tracks[-(max_tracks - 1):],
-            current_track_string
-        ]
+    def _to_stdout(self, data):
+        track_string = self._get_track_string(data)
+        if track_string:
+            print(track_string)
 
-    with open(outfile, mode) as f:
-        f.write('\n'.join(tracks_to_output))
+    def _create_outfile(self):
+        outpath = pathlib.Path(self.outfile)
+        outpath.parent.mkdir(parents=True, exist_ok=True)
+        outpath.touch(exist_ok=True)
+
+        if not outpath.is_file():
+            print(f'{self.outfile} is a directory!')
+            raise IsADirectoryError
+
+    def _to_file(self, data):
+        mode = 'a' if (self.append and self.max_tracks < 0)  else 'w'
+
+        current_track_string = self._get_track_string(data)
+        tracks_to_output = [current_track_string]
+
+        if self.max_tracks > 1:
+            with open(self.outfile, 'r') as f:
+                existing_tracks = f.read().splitlines()
+
+            tracks_to_output = [
+                *existing_tracks[-(self.max_tracks - 1):],
+                current_track_string
+            ]
+
+        with open(self.outfile, mode) as f:
+            f.write('\n'.join(tracks_to_output))
+
 
 class Listener():
     """Listens to Traktor broadcast, given a port."""
@@ -84,37 +115,19 @@ class Listener():
         self.max_tracks = max_tracks
         self.custom_callback = custom_callback
 
-    def _create_outfile(self):
-        if not self.outfile:
-            return
-        outpath = pathlib.Path(self.outfile)
-        outpath.parent.mkdir(parents=True, exist_ok=True)
-        outpath.touch(exist_ok=True)
-
-        if not outpath.is_file():
-            print(f'{self.outfile} is a directory!')
-            raise IsADirectoryError
-
     def start(self):
         """Start listening to Traktor broadcast."""
 
         callbacks = []
 
+        if self.outfile is not None or not self.quiet:
+            writer = TrackWriter(self.quiet, self.outfile, self.append, self.max_tracks)
+            callbacks.append(writer.update)
+
         if not self.quiet:
             print(f'Listening on port {self.port}.')
             if self.outfile:
                 print(f'Outputting to {self.outfile}')
-            callbacks.append(_output_to_console)
-
-        if self.outfile:
-            try:
-                self._create_outfile()
-                callbacks.append(functools.partial(
-                    _output_to_file, outfile=self.outfile, append=self.append, max_tracks=self.max_tracks
-                ))
-            except:
-                print(f'Error encountered while trying to write to {self.outfile}.')
-                return
 
         if self.custom_callback:
             callbacks.append(self.custom_callback)
